ad2bf7130e630ea1255a67b71e7c0990d0b4277c
Paul Llanque <llanquepol@gmail.com>
2023-09-17 19:20:29 -0500

    [ADD] config added

---
+++ config/environments/development.js
@@ -0,0 +1,10 @@
+module.exports = {
+  PORT: process.env.PORT,
+  DB: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_DEV,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+};
---
+++ config/environments/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PRODUCTION = require('./production');
+const DEVELOPMENT = require('./development');
+const QA = require('./qa');
+
+const { NODE_ENV } = process.env;
+
+let currentEnv = DEVELOPMENT;
+
+if (NODE_ENV === 'production') currentEnv = PRODUCTION;
+if (NODE_ENV === 'qa') currentEnv = QA;
+
+module.exports = currentEnv;
