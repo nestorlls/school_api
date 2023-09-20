180578978a621ff287f4af566890dc56a75e28cb
Paul Llanque <llanquepol@gmail.com>
2023-09-20 18:06:21 -0500

    [UPDATE] class userController updated

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,17 @@
 class UserController {
-  sayHello(req, res) {
-    return res.send({ message: 'Hello World!' });
+  constructor({ UserService }) {
+    this._userService = UserService;
+  }
+
+  async createUser(req, res) {
+    const body = req.body;
+    const createdUser = await this._userService.createUser(body);
+    return res.send({ error: false, payload: createdUser });
+  }
+
+  async getUsers(req, res) {
+    const users = await this._userService.getUsers();
+    return res.send({ error: false, payload: users });
   }
 }
 
