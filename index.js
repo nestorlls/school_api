f5bd2ea47d6884433d173e7c7d0b2087f407b694
Paul Llanque <llanquepol@gmail.com>
2023-09-20 18:07:19 -0500

    [ADD] comunication with db added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const container = require('./api/container');
 
 const app = container.resolve('app');
+const db = container.resolve('db');
 
 app
   .start()
-  .then(() => {
+  .then(async () => {
     console.log('app started');
+
+    await db.sequelize.sync();
   })
   .catch((err) => {
     console.log(err);
