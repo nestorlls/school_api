b2b80a838f1d60bb803c09f01b3240977b9adeac
Paul Llanque <llanquepol@gmail.com>
2023-09-20 18:06:45 -0500

    [UPDATE] user routes updated

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,7 +3,8 @@ const { Router } = require('express');
 module.exports = ({ UserController }) => {
   const router = Router();
 
-  router.get('/', UserController.sayHello);
+  router.get('/', UserController.getUsers.bind(UserController));
+  router.post('/', UserController.createUser.bind(UserController));
 
   return router;
 };
