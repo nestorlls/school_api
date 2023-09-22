09c09c571908d028510c5a9587c67967a49671b4
Paul Llanque <llanquepol@gmail.com>
2023-09-21 19:39:41 -0500

    [DELETE] DTOs deleted

--- api/dtos/Course.dto.js
+++
@@ -1,9 +0,0 @@
-class Course {
-  id = 0;
-  name = '';
-  status = '';
-  createdAt = null;
-  updatedAt = null;
-}
-
-module.exports = Course;
--- api/dtos/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  TeacherDto: require('./Teacher.dto'),
-  SubjectDto: require('./Subject.dto'),
-  StudentDto: require('./Student.dto'),
-  SectionDto: require('./Section.dto'),
-  RegistrationDto: require('./Registration.dto'),
-  CourseDto: require('./Course.dto'),
-};
