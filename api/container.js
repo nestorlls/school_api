2d274f5ddacd8fa8815079cd82f87b4e6b7b2a8f
Paul Llanque <llanquepol@gmail.com>
2023-09-23 22:09:48 -0500

    [ADD] dependencies added in container

--- api/container.js
+++ api/container.js
@@ -6,20 +6,59 @@ const Server = require('./server');
 // config
 const config = require('../config/environments/index');
 
-// controllers
-const { UserController } = require('./controllers');
-
-// services
-const { UserService } = require('../services');
-
-// repositories
-const { UserRepository } = require('../dal/repositories');
-
-const db = require('../dal/entities');
-
 // routes
 const Routes = require('./routes/index');
-const userRoutes = require('./routes/user.routes');
+const {
+  TeacherRoutes,
+  StudentRoutes,
+  CourseRoutes,
+  SectionRoutes,
+  SubjectRoutes,
+  RegistrationRoutes,
+} = require('./routes/index.routes');
+
+// controllers
+const {
+  TeacherController,
+  StudentController,
+  CourseController,
+  RegistrationController,
+  SectionController,
+  SubjectController,
+} = require('./controllers');
+
+// services
+const {
+  TeacherService,
+  StudentService,
+  CourseService,
+  RegistrationService,
+  SectionService,
+  SubjectService,
+} = require('../services');
+// business
+
+const {
+  TeacherBusiness,
+  StudentBusiness,
+  CourseBusiness,
+  RegistrationBusiness,
+  SectionBusiness,
+  SubjectBusiness,
+} = require('../domain');
+
+// repositories
+const {
+  TeacherRepository,
+  StudentRepository,
+  CourseRepository,
+  RegistrationRepository,
+  SectionRepository,
+  SubjectRepository,
+} = require('../dal/repositories');
+
+// db
+const db = require('../dal/models/index');
 
 const container = createContainer();
 
@@ -31,19 +70,48 @@ container.register({
 });
 
 container.register({
-  UserController: asClass(UserController).singleton(),
+  TeacherRoutes: asFunction(TeacherRoutes).singleton(),
+  StudentRoutes: asFunction(StudentRoutes).singleton(),
+  CourseRoutes: asFunction(CourseRoutes).singleton(),
+  SectionRoutes: asFunction(SectionRoutes).singleton(),
+  SubjectRoutes: asFunction(SubjectRoutes).singleton(),
+  RegistrationRoutes: asFunction(RegistrationRoutes).singleton(),
 });
 
 container.register({
-  UserRoutes: asFunction(userRoutes).singleton(),
+  TeacherController: asClass(TeacherController).singleton(),
+  StudentController: asClass(StudentController).singleton(),
+  CourseController: asClass(CourseController).singleton(),
+  SectionController: asClass(SectionController).singleton(),
+  SubjectController: asClass(SubjectController).singleton(),
+  RegistrationController: asClass(RegistrationController).singleton(),
 });
 
 container.register({
-  UserService: asClass(UserService).singleton(),
+  TeacherService: asClass(TeacherService).singleton(),
+  StudentService: asClass(StudentService).singleton(),
+  CourseService: asClass(CourseService).singleton(),
+  SectionService: asClass(SectionService).singleton(),
+  SubjectService: asClass(SubjectService).singleton(),
+  RegistrationService: asClass(RegistrationService).singleton(),
 });
 
 container.register({
-  UserRepository: asClass(UserRepository).singleton(),
+  TeacherBusiness: asClass(TeacherBusiness).singleton(),
+  StudentBusiness: asClass(StudentBusiness).singleton(),
+  CourseBusiness: asClass(CourseBusiness).singleton(),
+  SectionBusiness: asClass(SectionBusiness).singleton(),
+  SubjectBusiness: asClass(SubjectBusiness).singleton(),
+  RegistrationBusiness: asClass(RegistrationBusiness).singleton(),
+});
+
+container.register({
+  TeacherRepository: asClass(TeacherRepository).singleton(),
+  StudentRepository: asClass(StudentRepository).singleton(),
+  CourseRepository: asClass(CourseRepository).singleton(),
+  SectionRepository: asClass(SectionRepository).singleton(),
+  SubjectRepository: asClass(SubjectRepository).singleton(),
+  RegistrationRepository: asClass(RegistrationRepository).singleton(),
 });
 
 container.register({
